7b08add8e217d243ac1ed293638ba9a90fda7c41
chi <10656024@ntub.edu.tw>
2021-12-16 14:27:23 +0800

    110506

---
+++ public/README.txt
@@ -0,0 +1,1 @@
+Run the index.html to view the banner
---
+++ public/init-firebase.js
@@ -0,0 +1,14 @@
+// TODO: Replace the following with your app's Firebase project configuration
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA3bmTBDmYE2AL4Hucvjx4BvuOXruQB-kQ",
+    authDomain: "ntubimd110506.firebaseapp.com",
+    databaseURL: "https://ntubimd110506-default-rtdb.firebaseio.com/",
+    projectId: "ntubimd110506",
+    storageBucket: "gs://ntubimd110506.appspot.com/",
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
